e63811d54b4fa74357e448425fda69ad30c59292
ssyyd15 <ssyyd15@LAPTOP-GNFAOSFD>
2026-04-26 15:53:24 +0800

    updated preliminary task

--- Yiwen_Deng_20721088.m
+++ Yiwen_Deng_20721088.m
@@ -1,10 +1,17 @@
 % Yiwen Deng
 % ssyyd15@nottingham.edu.cn
-
+clear
 
 %% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [5 MARKS]
 
-% Insert answers here
+a=arduino('COM5','Uno'); % assign a variable a, where the port is COM5 and the type of Arduino is Uno
+
+for n=1:100 % define the initial repeating times, can change
+    writeDigitalPin(a,'D3',1) % lighten LED, the long LED log sits to the digital channel 3 of the Arduimo
+    pause(0.5) % pause 0.5 s
+    writeDigitalPin(a,'D3',0) % switch off LED
+    pause(0.5)
+end % end of loop over n
 
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
 
